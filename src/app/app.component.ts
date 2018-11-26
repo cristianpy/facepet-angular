582c9f29f859489a7f6f8ccd4b0785c8141613d4
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2018-11-26 14:17:12 -0300

    Se agrego una descripcion relacionada a Facepet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facepet-angular';
+  title = 'FacePet - La nueva red social de mascotas';
 }
